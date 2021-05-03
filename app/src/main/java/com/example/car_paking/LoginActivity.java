823f83b86c845746a2b3cfaa70de37a0d9cb1abc
memorybuqingyu <64903770+memorybuqingyu@users.noreply.github.com>
2021-05-03 17:18:33 +0800

    park1

--- app/src/main/java/com/example/car_paking/LoginActivity.java
+++ app/src/main/java/com/example/car_paking/LoginActivity.java
@@ -44,7 +44,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//
         setContentView(R.layout.activity_login);
         initView();
     }
